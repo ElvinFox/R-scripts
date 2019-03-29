3598139665142d099d224471a6009d8aa75f747c
ElvinFox <ElvinFox@yandex.ru>
2019-03-29 17:42:24 +0300

    Update OZN.R

--- OZN.R
+++ OZN.R
@@ -4,3 +4,67 @@ library(tibble)
 library(lubridate) 
 library(sjmisc)
 library(gtools)
+
+
+
+### https://www.ozon.ru/category/7500/
+### https://www.ozon.ru/category/11424/ 
+    
+# 28 SKU per page
+# not clear how the site deals with ?pages
+
+
+input <- "https://www.ozon.ru/category/7500/"
+
+url <- paste0(input, "?page=",i )
+OZN <- url %>% read_html()
+
+
+nmbr <- 
+    OZN %>% 
+    html_nodes(".category-count") %>% 
+    html_text()
+gsub("[^0-9]", "", nmbr)
+
+# find depth for scrapping
+max_pages <- nmbr/28
+
+prod <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_nodes(".tile") %>% 
+    html_text()
+
+
+prod_img <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_nodes(".tile") %>% 
+    html_nodes(".img-inner-wrapper") %>% 
+    html_text()
+
+prod_price <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_nodes(".price-container") %>% 
+    html_nodes(".img-inner-wrapper") %>% 
+    html_text()
+
+prod_id <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_attr(".id") %>% 
+    html_text()
+
+prod_id <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_attr(".id") %>% 
+    html_text()
+
+
+prod_name <- 
+    OZN %>% 
+    html_nodes(".tiles") %>% 
+    html_attr(".name") %>% 
+    html_text()
