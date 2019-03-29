f3742c39d31e9bd5966c2ed91eda9df96e7a41f6
ElvinFox <ElvinFox@yandex.ru>
2019-03-29 17:42:55 +0300

    Update SM.R

--- SM.R
+++ SM.R
@@ -7,6 +7,7 @@ library(gtools)
 
 
 
+
 # # Clothes
 # 
 #     https://www.sportmaster.ru/catalog/muzhskaya_odezhda/
@@ -19,7 +20,29 @@ library(gtools)
 #     https://www.sportmaster.ru/catalog/obuv_dlya_malchikov/ 
 #     https://www.sportmaster.ru/catalog/obuv_dlya_devochek/ 
 #
+
+
+# https://www.sportmaster.ru/
+
+
+list_urls <- 
+    SMR %>% 
+    html_nodes(".newSubmenuTitle a") %>% 
+    html_text()
+
+
+
+
+
+
+
+
 # ?pageSize=120
+
+
+
+front <- data.frame()
+begin <- Sys.time()
 
 
 # response delay from the website
@@ -33,3 +56,87 @@ for(my_url in my_urls){
     response_delay <- as.numeric(t1-t0)
     Sys.sleep(10*response_delay) # sleep 10 times longer than response_delay
 }
+
+input <- "https://www.sportmaster.ru/catalog/muzhskaya_odezhda/"
+
+url <- paste0(input, "?pageSize=120&page=",i )
+SMR <- url %>% read_html()
+
+prod_img <- 
+    SMR %>% 
+    html_nodes(".sm-category__item-photo img") %>% 
+    html_text()
+
+
+prod_name <- 
+    SMR %>% 
+    html_nodes(".sm-category__item h2 a") %>% 
+    html_text()
+
+prod_id <- 
+    SMR %>% 
+    html_nodes(".sm-category__item-photo a") %>% 
+    html_text()
+
+
+# https://www.sportmaster.ru/product/10332081/
+
+paste0("https://www.sportmaster.ru", prod_id)
+
+sf <- cbind(prod_id, prod_img, prod_category, prod_link)
+sf <- as.data.frame(sf)
+front <- smartbind(front, sf)
+
+
+
+
+
+
+
+tree <- 
+    SMR %>% 
+    html_nodes(".sm-breadcrumbs") %>% 
+    html_text()
+
+
+tree <- gsub("[\t \n]", "", tree)
+
+
+
+# price main, old
+
+price_new <- 
+    SMR %>% 
+    html_nodes(".sm-amount") %>% 
+    html_text()
+price_new <- as.numeric( gsub("[^0-9]", "", price_new[1]) )
+
+price_old <- 
+    SMR %>% 
+    html_nodes(".wr_item_3_old_price") %>% 
+    html_text()
+price_old <- as.numeric( gsub("[^0-9]", "", price_old[1]) )
+
+
+sizes <- 
+    SMR %>% 
+    html_nodes(".clearfix div") %>% 
+    html_nodes(".sm-goods__param-value") %>% 
+    html_text()
+
+
+descript <- 
+    SMR %>%
+    html_nodes (".sm-goods__description-text")
+
+characteristics <- 
+    SMR %>%
+    html_nodes (".sm-goods_tabs-bodies_body") %>% 
+    html_nodes (".sm-goods_tabs_characteristix") %>% 
+    html_nodes (".characteristics_values") %>% 
+    html_table ()
+
+color <- 
+    SMR %>%
+    html_nodes (".sm-goods_main_details_color a") %>% 
+    html_text()
